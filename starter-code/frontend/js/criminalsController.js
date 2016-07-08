1c67155d8d9782e5b694af7a7e353ecce3e36da9
Limelight-Management-Group <thestorerun@yahoo.com>
2016-07-07 19:02:59 -0700

    resource commit

--- starter-code/frontend/js/criminalsController.js
+++ starter-code/frontend/js/criminalsController.js
@@ -1,27 +1,50 @@
-angular.module('infamousCriminalsApp', [])
-  .controller('CriminalsController', CriminalsController);
-CriminalsController.$inject = ['$http'];
-function CriminalsController($http){
-  this.all = []
-  this.addCriminal = addCriminal;
-  this.newCriminal = {};
-  this.getCriminals = getCriminals
-  var self = this
-  function addCriminal(){
-    $http.post('http://localhost:3000/criminals', self.newCriminal)
-    .then(function(response) {
-      this.newCriminal = {};
-    })
-    this.all.push(this.newCriminal);
-  }
-function getCriminals() {
-  $http.get('http://localhost:3000/criminals')
-    .then(function(response) {
-      console.log(response);
-      self.all = response.data.criminals
+angular.module('infamousCriminalsApp').factory('Criminals', function($resource) {
+  return $resource('localhost:3000/criminals/:id', null,
+    {
+      update: {method: 'PUT' },
+      query: {isArray: false}
     });
-};
+});
 
 
-  getCriminals();
-};
+angular.module('infamousCriminalsApp').controller('CriminalsController', CriminalsController);
+
+
+
+CriminalsController.$inject = ['$resource'];
+function CriminalsController(Criminals){
+  
+  // this.addCriminal = addCriminal;
+  // this.newCriminal = {};
+  var self = this;
+  self.all = [];
+  // this.getCriminals = getCriminals
+  // self.deleteCriminal = deleteCriminal;
+  
+  var response = Criminals.query(function() {
+    self.all = response.criminals
+  });
+  // function deleteCriminal(criminal) {
+  //   Criminals.delete({ id: criminal._id});
+  // };
+  // function addCriminal(){
+  //   $http.post('http://localhost:3000/criminals', self.newCriminal)
+  //   .then(function(response) {
+  //     self.all.push(self.newCriminal);
+  //     self.newCriminal = {};
+  //   })
+  // }
+
+  // function getCriminals() {
+  //   $http.get('http://localhost:3000/criminals')
+  //     .then(function(response) {
+  //       console.log(response);
+  //       self.all = response.data.criminals
+  //     });
+  // };
+  // getCriminals();
+  
+  }
+
+
+
